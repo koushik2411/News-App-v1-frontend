55453a079a778d3a06de445145341e1463639e5a
koushik2411 <koushikrk1324@gmail.com>
2026-03-31 12:47:36 +0530

    Fixed News Card

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -6,8 +6,8 @@ const NewsCard = ({article}) => {
     <>
         <div className=' w-full p-3 rounded-lg bg-amber-200 flex flex-col gap-2 justify-evenly bg-linear-to-b from-gray-50 to-gray-200 dark:bg-linear-to-b dark:from-gray-900 dark:to-gray-800 dark:text-gray-100'>
             <img
-                src={article.image ? article.image  : "https://via.placeholder.com/300"}
-                alt='newsImg'
+                src={article.image && article.image !== "None" ? article.image  : "https://via.placeholder.com/300"}
+                alt='No image'
                 className=' rounded-t-lg lg:h-56'
             />
 
@@ -20,10 +20,10 @@ const NewsCard = ({article}) => {
             </p>
 
             <p>
-                {article.author || "Unknown"}
+                {article.author && article.author !== "None" ? article.author : "Unknown"}
             </p>
 
-            <a href={article.url} target='_blank' rel='noreferrer' className=' text-gray-100 self-end'>
+            <a href={article.url && article.url !== "None" ? article.url : "#"} target='_blank' rel='noreferrer' className=' text-gray-100 self-end'>
                 <button className=' py-1 px-3 rounded-lg bg-linear-to-b from-gray-500 to-gray-700 dark:bg-linear-to-b dark:from-gray-600 dark:to-gray-700 flex gap-2 items-center'>
                     Read More
                     <FaArrowCircleRight />
